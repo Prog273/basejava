56622b48c23b79d4394862be0145c5dd3a22453d
Dima M. <dimitrius273@mail.ru>
2025-01-21 22:36:34 +0300

    Updated ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,46 +3,47 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = size();
 
     void clear() {
-        for (int i = 0; i < this.size(); i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
     }
 
     void save(Resume r) {
-        storage[this.size()] = r;
+        storage[size] = r;
     }
 
     Resume get(String uuid) {
-        Resume necessaryResume = new Resume();
-        for (var resume : this.getAll()) {
+        for (var resume : getAll()) {
             if (resume.uuid.equals(uuid)) {
-                necessaryResume = resume;
-                break;
+                return resume;
             }
         }
-        return necessaryResume;
+        return null;
     }
 
     void delete(String uuid) {
+        //следующая проверка - чтобы не было ArrayIndexOutOfBoundsException:
+        if (size == 0) return;
+
         int indexOfDeletedResume = 0;
-        for (int i = 0; i < this.size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 indexOfDeletedResume = i;
+                break;
             }
         }
-        var allResumes = this.getAll();
-        for (int i = indexOfDeletedResume; i < allResumes.length - 1; i++) {
-            storage[i] = allResumes[i + 1];
-        }
+        storage[indexOfDeletedResume] = storage[size - 1];
+        storage[size - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] allResumes = new Resume[this.size()];
+        Resume[] allResumes = new Resume[size];
         for (int i = 0; i < allResumes.length; i++) {
             allResumes[i] = storage[i];
         }
