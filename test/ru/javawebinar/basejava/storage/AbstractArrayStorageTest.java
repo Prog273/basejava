c5b6b765be36fd23de989a58833446433d6c472b
Dima M. <dimitrius273@mail.ru>
2025-02-17 02:46:49 +0300

    Changed for exceptions and reflection (hw4)

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -1,12 +1,13 @@
 package ru.javawebinar.basejava.storage;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +17,12 @@ abstract class AbstractArrayStorageTest {
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+    private static final String UUID_4 = "uuid4";
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_4 = new Resume(UUID_4);
+
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -24,115 +31,118 @@ abstract class AbstractArrayStorageTest {
     @BeforeEach
     public void setUp() throws Exception {
         storage.clear();
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_2));
-        storage.save(new Resume(UUID_3));
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
     }
 
     @Test
-    @DisplayName("clear() очищает массив с Resume")
-    void clearShouldClearStorage() {
+    void clearClearsStorage() {
         storage.clear();
         assertEquals(0, storage.size(), "expected storage to be empty");
+        Resume[] emptyArr = storage.getAll();
+        boolean areAllElementsNull = Arrays.stream(emptyArr)
+                .allMatch(element -> element == null);
+        assertTrue(areAllElementsNull);
     }
 
     @Test
-    @DisplayName("update() заменяет Resume при наличии UUID")
-    void updateShouldUpdateResume() {
-        Resume oldResume = storage.get(UUID_3);
-        Resume newResume = new Resume(UUID_3);
+    void updateUpdatesResume() {
+        Resume newResume = RESUME_3;
         storage.update(newResume);
-        assertEquals(oldResume, storage.get(UUID_3));
+        assertSame(newResume, storage.get(UUID_3));
     }
 
     @Test
-    @DisplayName("update() выбрасывает NotExistStorageException при несуществующем Resume")
-    void updateShouldThrowException() {
-        Resume newResume = new Resume("uuid4");
+    void updateNotExisting() {
+        Resume newResume = RESUME_4;
         assertThrows(NotExistStorageException.class, () -> storage.update(newResume));
     }
 
-    @Test
-    @DisplayName("get() возвращает Resume при наличии UUID")
-    void getShouldReturnResume() {
-        Resume newResume = new Resume(UUID_3);
-        assertEquals(newResume, storage.get(UUID_3));
+//    @ParameterizedTest
+    void assertGet(Resume resume) {
+        assertEquals(resume, storage.get(resume.getUuid()));
     }
 
     @Test
-    @DisplayName("get() выбрасывает NotExistStorageException при несуществующем Resume")
-    void getShouldThrowException() {
-        Resume newResume = new Resume("uuid4");
+    void getReturnsResume() {
+        Resume newResume = RESUME_3;
+        assertGet(newResume);
+    }
+
+    @Test
+    void getNotExisting() {
+        Resume newResume = RESUME_4;
         assertThrows(NotExistStorageException.class, () -> storage.get(newResume.getUuid()));
     }
 
     @Test
-    @DisplayName("delete() удаляет резюме из хранилища")
-    void deleteShouldDeleteResume() {
+    void deleteDeletesResume() {
+        /** Не стал тут переименовывать в deleteNotExisting, так как цель этого теста не проверить выброс исключения,
+         а именно проверить, удалился ли элемент. Для этого и использую исключение. В таком случае, надо переименовать?
+         */
         storage.delete(UUID_1);
         assertThrows(NotExistStorageException.class, () -> storage.get(UUID_1),
-                "Резюме с UUID1 должно отсутствовать после удаления");
+                "There must be no resume with UUID_1 after deleting");
+    }
+
+//    @ParameterizedTest
+    void assertSize(int size) {
+        assertEquals(size, storage.size());
     }
 
     @Test
-    @DisplayName("delete() уменьшает размер базы данных резюме")
-    void deleteShouldReduceSize() {
+    void deleteReducesSize() {
         storage.delete(UUID_1);
-        assertEquals(2, storage.size());
+        assertSize(2);
+        assertThrows(NotExistStorageException.class, () -> assertGet(RESUME_1));
     }
 
     @Test
-    @DisplayName("delete() выбрасывает NotExistStorageException при пустом хранилище")
-    void deleteShouldThrowException1() {
+    void deleteNotExistingFromEmptyStorage() {
         storage.clear();
         assertThrows(NotExistStorageException.class, () -> storage.delete(UUID_1));
     }
 
     @Test
-    @DisplayName("delete() выбрасывает NotExistStorageException при отсутствии Resume в хранилище")
-    void deleteShouldThrowException2() {
-        Resume newResume = new Resume("uuid4");
+    void deleteNotExistingResume() {
+        Resume newResume = RESUME_4;
         assertThrows(NotExistStorageException.class, () -> storage.delete(newResume.getUuid()));
     }
 
     @Test
-    @DisplayName("getAll() возвращает массив со всеми резюме")
-        //не работает!
-    void getAllShouldReturnResumes() {
-        Resume[] allResumes = new Resume[]{storage.get(UUID_1), storage.get(UUID_2), storage.get(UUID_3)};
-        assertEquals(allResumes, storage.getAll());
+    void getAllReturnsResumes() {
+        Resume[] allResumes = new Resume[]{RESUME_1, RESUME_2, RESUME_3};
+        assertArrayEquals(allResumes, storage.getAll());
     }
 
     @Test
-    @DisplayName("size() возвращает размер базы данных резюме")
     void size() {
-        assertEquals(3, storage.size());
+        assertSize(3);
     }
 
     @Test
-    @DisplayName("save() выбрасывает StorageException при переполнении")
-    void saveShouldThrowStorageException() {
+    void saveOverflow() {
+        storage.clear();
         try {
-            for (int i = 4; i <= STORAGE_LIMIT; i++) {
+            for (int i = 1; i <= STORAGE_LIMIT; i++) {
                 storage.save(new Resume("uuid" + i));
             }
         } catch (StorageException e) {
-            fail("Переполнение массива произошло раньше времени");
+            fail("The array overflow happened earlier than expected.");
         }
         assertThrows(StorageException.class, () -> storage.save(new Resume("uuid" + (STORAGE_LIMIT + 1))));
     }
 
     @Test
-    @DisplayName("save() выбрасывает ExistStorageException при попытке сохранить уже имеющееся резюме")
-    void saveShouldThrowExistStorageException() {
-        assertThrows(ExistStorageException.class, () -> storage.save(new Resume(UUID_2)));
+    void saveExisting() {
+        assertThrows(ExistStorageException.class, () -> storage.save(RESUME_2));
     }
 
     @Test
-    @DisplayName("save() увеличивает размер базы данных резюме")
-    void saveShouldIncreaseSize() {
-        storage.save(new Resume("uuid4"));
-        assertEquals(4, storage.size());
+    void saveIncreasesSize() {
+        storage.save(RESUME_4);
+        assertSize(4);
     }
 
     @Test
